40cfad87a3121c0b899437c7a8835ecc4d56e18a
jiobr4178-png <jiobr4178@gmail.com>
2025-08-20 19:58:02 -0700

    Add files via upload

---
+++ supabase/migrations/20250820004035_81b9396c-7cc1-4585-9574-6ebff57b3a9c.sql
@@ -0,0 +1,11 @@
+-- Fix the search_path security issue by updating the function
+DROP FUNCTION IF EXISTS public.update_updated_at_column();
+
+CREATE OR REPLACE FUNCTION public.update_updated_at_column()
+RETURNS TRIGGER AS $$
+BEGIN
+  NEW.updated_at = now();
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql
+SECURITY DEFINER SET search_path = public;
